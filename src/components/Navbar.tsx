1d6e024c0e9d3c95f465d730cabf2678e1b48e75
aurorakennedy <aurorke@stud.ntnu.no>
2023-11-06 10:43:39 +0200

    Remove react imports

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-
 const Navbar = () => {
-    return (
-        <nav className="background-color: rgb(190 242 100); bg-white border-gray-200 dark:bg-gray-900">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"></div>
-          <div className="absolute text-center left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-2 px-4  ">
-            <a href="/">
-            <span className="font-rock font-bold text-7xl text-pink-600">Aurora Kennedy</span>
-            </a>
-          </div>
-              
-              {/* <div className="hidden md:block">
+  return (
+    <nav className="background-color: rgb(190 242 100); bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"></div>
+      <div className="absolute text-center left-1/2 transform -translate-x-1/2 -translate-y-1/2 py-2 px-4  ">
+        <a href="/">
+          <span className="font-rock font-bold text-7xl text-pink-600">
+            Aurora Kennedy
+          </span>
+        </a>
+      </div>
+
+      {/* <div className="hidden md:block">
                 <div className="ml-4 flex items-center md:ml-6">
                   <a
                     href="#"
@@ -32,9 +32,8 @@ const Navbar = () => {
                   </a>
                 </div>
               </div> */}
-          
-        </nav>
-      );
-    };
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
