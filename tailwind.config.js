adbe4865e2c629a18053e3f98c1f5af43fa7cc94
aurorakennedy <aurorke@stud.ntnu.no>
2023-11-07 09:24:33 +0200

    add google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+        rock: "'Rock 3D', cursive",
+        handwriting: " 'Annie Use Your Telescope', cursive",
+      },
+    },
   },
   plugins: [],
 };
