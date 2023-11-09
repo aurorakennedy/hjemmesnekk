e4a8245967c07f7eaf2d6028215f68d5074dec9f
aurorakennedy <aurorke@stud.ntnu.no>
2023-11-09 14:50:31 +0200

    update about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,40 +1,28 @@
+import Navbar from "../components/Navbar";
+
 const About = () => {
   return (
     <>
       <body>
         {/* Bakgrunn */}
-        <main className="bg-purple-500">
+        <main className="bg-purple-200">
+          <Navbar />
           <div className="text-center pt-10">
-            <h1 className="text-3xl">Bli kjent med meg:</h1>
+            <h1 className="text-2xl font-playpen font-semibold">About:</h1>
             <br />
-
-            <p>Jeg må sove med sovemaske</p>
+            <p className=" px-56">
+              Hi, I'm Aurora, an informatics student. I created this webpage to
+              practice what I'm learning. Usually based in Trondheim, but right
+              now, I'm on an exchange semester in Athens ☀️.
+            </p>
             <img
-              className="inline object-scale-down h-50 w-96 rounded-md"
-              src="/images/sovemaske.jpeg"
+              className="inline object-scale-down h-50 w-96 pt-12 rounded-md"
+              src="/images/akropolis.jpeg"
               alt="A beautiful landscape"
             ></img>
-
-            <p>Jeg er forelska</p>
-            <img
-              className="inline object-scale-down h-50 w-96 rounded-md"
-              src="/images/grain.jpeg"
-              alt="A beautiful landscape"
-            ></img>
-
-            <p>Jeg elsker nudler </p>
-            <img
-              className="inline object-scale-down h-50 w-96 rounded-md"
-              src="/images/nudler.jpeg"
-              alt="A beautiful landscape"
-            ></img>
-
-            <p>Drunk eyes</p>
-            <img
-              className="inline object-scale-down h-50 w-96 rounded-md"
-              src="/images/drunkeyes.jpeg"
-              alt="A beautiful landscape"
-            ></img>
+            <br />
+            <br />
+            <br /> <br /> <br />
           </div>
         </main>
       </body>
