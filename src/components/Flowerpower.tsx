55b795fcaf3fe5aa53e67ace6e62e5981707c889
aurorakennedy <aurorke@stud.ntnu.no>
2023-11-13 19:11:09 +0200

    Make flower background to component

---
+++ src/components/Flowerpower.tsx
@@ -0,0 +1,99 @@
+const Flowerpower = () => {
+  return (
+    <div className="h-screen  bg-blue-200">
+      <div className="hidden sm:block absolute left-5 top-20 w-48 h-auto ">
+        <img
+          src="/images/blomster/lyseblå.png"
+          alt="Lyseblå blomst"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className=" hidden sm:block absolute right-20 top-6 w-32 h-auto">
+        <img
+          src="/images/blomster/lilla.webp"
+          alt="Druelilla m/gul"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden md:block absolute right-56 bg-center bottom-72 w-24 h-auto">
+        <img
+          src="/images/blomster/rosa.png"
+          alt="Lyserosa "
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden xl:block absolute right-80 top-56 w-48 h-auto">
+        <img
+          src="/images/blomster/blå.png"
+          alt="Mørkeblå"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden xl:block absolute left-80 top-5 w-48 h-auto">
+        <img
+          src="/images/blomster/orange.webp"
+          alt="Helt oransj"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden sm:block absolute bottom-9 right-5 w-48 h-auto ">
+        <img
+          src="/images/blomster/rosagrønn.png"
+          alt="Rosa grønn"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className=" hidden xl:block absolute bottom-9 left-72 w-44 h-auto ">
+        <img
+          src="/images/blomster/rød.png"
+          alt="Rød"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden xl:block absolute bottom-28 right-96 w-24 h-auto ">
+        <img
+          src="/images/blomster/blåbrun.png"
+          alt="Blå brun"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden xl:block absolute top-8 right-96 w-24 h-auto ">
+        <img
+          src="/images/blomster/grønn.png"
+          alt="Grønn"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden xl:block absolute top-56 left-96 w-32 h-auto ">
+        <img
+          src="/images/blomster/gulrød.png"
+          alt="Gul rød"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden md:block absolute top-56 right-28 w-32 h-auto ">
+        <img
+          src="/images/blomster/lilla2.png"
+          alt="Lilla gjennomsiktig"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden md:block absolute top-80 left-52 w-36 h-auto ">
+        <img
+          src="/images/blomster/lillagul.png"
+          alt="Lilla gul"
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="hidden md:block absolute bottom-36 left-16 w-40 h-auto ">
+        <img
+          src="/images/blomster/oransjblå.png"
+          alt="Oransj blå"
+          className="w-full h-full object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Flowerpower;
