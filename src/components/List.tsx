494dcd215000747c3cbdb1ca6b286506e371cc01
aurorakennedy <aurorke@stud.ntnu.no>
2023-11-13 19:11:44 +0200

    Make list component reusable

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,25 +1,25 @@
-interface Venn {
-  url: string;
+interface List {
+  url?: string;
   navn: string;
   bilde: string;
 }
 
 interface ListProps {
-  items: Venn[];
+  items: List[];
 }
 
 function List({ items }: ListProps) {
   return (
-    <div className=" text-center">
-      <ul className="flex flex-row justify-center gap-8">
-        {items.map((venn, index) => (
-          <li key={index}>
-            <a className="font-mono hover:text-pink-600 mb-10" href={venn.url}>
-              {venn.navn}
+    <div className="px-32 pt-20 text-center">
+      <ul className="flex flex-wrap justify-center gap-8">
+        {items.map((item, index) => (
+          <li key={index} className="mb-10">
+            <a className="font-mono hover:text-pink-600 mb-10" href={item.url}>
+              {item.navn}
               <img
-                className="h-64 rounded-lg transition-transform transform "
-                src={venn.bilde}
-                alt={`Bilde av ${venn.navn}`}
+                className="h-96 rounded-lg transition-transform transform "
+                src={item.bilde}
+                alt={`Bilde av ${item.navn}`}
               />
             </a>
           </li>
